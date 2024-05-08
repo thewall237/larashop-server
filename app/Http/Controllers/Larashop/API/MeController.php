c545a6855129f333f35f13dc6d8d428acc0d7bf5
thewall237 <outsidethewall237@gmail.com>
2024-05-08 11:49:46 +0900

    ✅ 05_10_ログインユーザー情報更新APIの実装

--- app/Http/Controllers/Larashop/API/MeController.php
+++ app/Http/Controllers/Larashop/API/MeController.php
@@ -37,11 +37,10 @@ class MeController extends Controller
      * @return void
      */
     public function __construct(
-        // UserServiceInterface $userService,
+        UserServiceInterface $userService,
         // ProductServiceInterface $productService
-    )
-    {
-        // $this->userService = $userService;
+    ) {
+        $this->userService = $userService;
         // $this->productService = $productService;
     }
 
@@ -64,16 +63,16 @@ class MeController extends Controller
      * @param  UpdateRequest  $request
      * @return MeResource
      */
-    // public function update(UpdateRequest $request)
-    // {
-    //     /** @var \App\Models\User $user */
-    //     $user = Auth::user();
-    //     $params = $request->safe()->toArray();
+    public function update(UpdateRequest $request)
+    {
+        /** @var \App\Models\User $user */
+        $user = Auth::user();
+        $params = $request->safe()->toArray();
 
-    //     $user = $this->userService->update($user, $params);
+        $user = $this->userService->update($user, $params);
 
-    //     return new MeResource($user);
-    // }
+        return new MeResource($user);
+    }
 
     /**
      * 購入商品一覧取得API
